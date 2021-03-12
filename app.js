2ba3e1075479c236aa58e72c8cf62b2b3f2d2944
Joshua <meyer.josh8@gmail.com>
2021-03-12 11:48:02 -0600

    Create Item constructor

--- app.js
+++ app.js
@@ -3,7 +3,14 @@
 // ITEM CONTROLLER
 // iffy function
 const ItemCtrl = (function(){
+  // Item constructor
+  const Item = function(id, name, calories){
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
 
+  // data structure / state
 })();
 
 // UI CONTROLLER
