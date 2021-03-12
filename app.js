a7958886e8c207f5eb1829ea9e6ac148b083fbb2
Joshua <meyer.josh8@gmail.com>
2021-03-12 11:46:38 -0600

    Create controllers

---
+++ app.js
@@ -0,0 +1,17 @@
+// STORAGE CONTROLLER
+
+// ITEM CONTROLLER
+// iffy function
+const ItemCtrl = (function(){
+
+})();
+
+// UI CONTROLLER
+const UICtrl = (function(){
+
+})();
+
+// APP CONTROLLER
+const AppCtrl = (function(ItemCtrl, UICtrl){
+
+})(ItemCtrl, UICtrl);
