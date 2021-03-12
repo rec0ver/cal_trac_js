56444891c5a1e1a7a0f934daf2e8be7c53d9db89
Joshua <meyer.josh8@gmail.com>
2021-03-12 11:51:46 -0600

    Create data/state structure and hardcode sample data

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const ItemCtrl = (function(){
   }
 
   // data structure / state
+  const data = {
+    items: [
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 400},
+      {id: 2, name: 'Eggs', calories: 300},
+    ],
+    // item to update
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  // public return
+  return {
+    
+  }
 })();
 
 // UI CONTROLLER
